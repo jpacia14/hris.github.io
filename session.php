15cb3b2dca83b54cdebac85174657d0d56944ff4
jpacia14 <125850087+jpacia14@users.noreply.github.com>
2023-08-16 09:28:58 +0800

    Update session.php

--- session.php
+++ session.php
@@ -11,7 +11,7 @@
    $login_session = $row['username'];
    
    if(!isset($_SESSION['user_login'])){
-      header("location:login.php");
+      header("location:index.php");
       die();
    }
 ?>
